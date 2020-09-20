cbdc087cfa7d41e79cc791b8cf2656c73f21bd47
metinoguzhann <metinoguzhann@gmail.com>
2020-09-21 02:11:37 +0300

    first commit

---
+++ 01-Php-Giris/05_Sihirli_Karakterler.php
@@ -0,0 +1,25 @@
+<?php
+
+
+    /**
+     *          SİHİRLİ KARAKTERLER
+     *  \n
+     *  \t
+     *  \\
+     *  \$
+     *  \'
+     *  \"
+     * 
+     */
+
+
+    $test = "Metin\t\t\tOğuzhan\nOğuzhan\\test";
+    echo $test;
+
+    echo "<br><br>";
+    $isim = "Metin Oğuzhan";
+    echo "Merhaba benim adım \"$isim\""."<br>";
+    echo 'Merhaba benim adım \'Metin\''."<br>";
+    echo "\$isim değişkeni \$isim değişkenine eşittir.";
+
+?>
