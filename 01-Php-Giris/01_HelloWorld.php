3805ad55072962b86b2c972f3200e9b42b46b673
metinoguzhann <metinoguzhann@gmail.com>
2020-09-21 01:33:28 +0300

    fist commit

--- 01-Php-Giris/01_HelloWorld.php
+++ 01-Php-Giris/01_HelloWorld.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?echo 'Metin Oğuzhan'?></title>
+    <title><?php echo 'Metin Oğuzhan' ?></title>
 </head>
 <body>
     <?php
