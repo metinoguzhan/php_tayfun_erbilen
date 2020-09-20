5f0dd4a95e2e32f9c7c3781c5e0716942f455ebb
metinoguzhann <metinoguzhann@gmail.com>
2020-09-21 01:28:25 +0300

    first commit

--- 01-Php-Giris/01_HelloWorld.php
+++ 01-Php-Giris/01_HelloWorld.php
@@ -1,5 +1,15 @@
-<?php
-
-    echo "Merhaba Dünya";
-    echo "Merhaba Dünya";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hello World !!!"."<br>";
+        print 'Merhaba Dünya<br>';
+        print 24;
+    ?>
+</body>
+</html>
