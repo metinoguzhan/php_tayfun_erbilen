ddbf9dd18c0bd52589c8fbad4c543899d149d0c9
unknown <metinoguzhann@gmail.com>
2020-09-26 00:47:29 +0300

    first commit

--- 02-Php-Operatorler/02_Arttirma_Operatorler.php
+++ 02-Php-Operatorler/02_Arttirma_Operatorler.php
@@ -14,7 +14,39 @@
      */
 
 
-     $a = 5;
+    $a = 5;
+    echo $a."<br>";
+    $a += 5;
+    echo $a."<br>";
+    $a -= 5;
+    echo $a."<br>";
+    $a *= 5;
+    echo $a."<br>";
+    $a /= 5;
+    echo $a."<br>";
+    $a %= 2;
+    echo $a."<br>";
 
 
+    $ad = "Metin";
+    $soyad = "Oguzhan";
+
+    echo $ad.$soyad;
+    echo "<br>";
+    echo "$ad $soyad";
+    echo "<br>";
+    echo "Metin"."34";
+    echo "<br><br><br>";
+
+    echo "Metin" . "Oğuzhan" . 24 . $ad . "Metin" . $soyad;
+    echo "<br><br><br>";
+
+    $ad .= "  Oğuzhan 35";
+    echo "$ad";
+
+    echo "<br><br><br>";
+
+    $ad = "Metin";
+    $ad .= " "."Oğuzhan";
+    echo $ad;
 ?>
