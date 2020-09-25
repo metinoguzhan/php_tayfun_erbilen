367ebb5baa0a6b9e7ff5bf3491c0570e1d2f9523
unknown <metinoguzhann@gmail.com>
2020-09-26 00:33:39 +0300

    first commit

---
+++ 02-Php-Operatorler/02_Arttırma_Operatorler.php
@@ -0,0 +1,20 @@
+<?php
+
+    /**
+     *           ATAMA OPERATORLERI
+     *   = değer atama
+     *  += artırarak değer atama
+     *  -= azaltarak değer atama
+     *  /= bölerek değer atama
+     *  *= çarparak değer atama
+     *  %= modunu alarak değer atama 
+     *  . birleştirme operatörü
+     *  .= birleştirerek değer atama
+     * 
+     */
+
+
+     $a = 5;
+
+
+?>
