4790c4198691676575dd1733805c899cf87262f2
unknown <metinoguzhann@gmail.com>
2020-09-26 00:27:22 +0300

    first commit

--- 02-Php-Operatorler/01_Aritmetik_Operatorler.php
+++ 02-Php-Operatorler/01_Aritmetik_Operatorler.php
@@ -1,27 +1,53 @@
 <?php
 
-/**
- * 
- *      ARITMETIK OPERATORLER
- * Toplama +
- * Çıkarma -
- * Çarpma *
- * Bölme /
- * Mod (Kalan) %
- * 
- *
- */
+    /**
+     * 
+     *      ARITMETIK OPERATORLER
+     * Toplama +
+     * Çıkarma -
+     * Çarpma *
+     * Bölme /
+     * Mod (Kalan) %
+     * 
+     *
+     */
+
+    $a = 10;
+    $b = 5;
+    echo $a + $b;
+    echo "<br/>";
+    echo $a - $b;
+    echo "<br/>";
+    echo $a / $b;
+    echo "<br/>";
+    echo $a * $b;
+    echo "<br/>";
+    echo 5 % 3;
+    echo "<br/>";
 
 
-echo 15 + 5;
-echo "<br>";
-echo 20 - 5;
-echo "<br>";
-echo 10 * 5;
-echo "<br>";
-echo 20 / 5;
-echo "<br>";
-echo 20 % 3;
+    echo "\tMatematik İşlem Önceliği <br>";
+    echo "5 + 2 * 5 * 2 / 5 = ";
+    echo 5 + 2 * 5 * 2 / 5;
+    echo "<br>";
+    echo "(5 + 2) * 5 * 2 / 5 = ";
+    echo (5 + 2) * 5 * 2 / 5;
+
+    echo "<br>";
+    echo "(5 + ((2 * 5) * 2)) / 5 = ";
+    echo (5 + ((2 * 5) * 2)) / 5;
+
+    echo "<br><br>";
+    $a = 5;
+    $b = 2;
+    $c = 5;
+    $d = 2;
+    $e = 5;
+
+    echo "($a + ($b * $c)) * ($d / $e) = ";
+    echo ($a + ($b * $c)) * ($d / $e);
+    
+    
 
 
 ?>
