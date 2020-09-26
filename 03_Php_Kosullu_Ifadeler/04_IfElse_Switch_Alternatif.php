7e78f842c6368aeb2a27ce0c484d233c17553e70
unknown <metinoguzhann@gmail.com>
2020-09-27 00:34:43 +0300

    first commit

---
+++ 03_Php_Kosullu_Ifadeler/04_IfElse_Switch_Alternatif.php
@@ -0,0 +1,37 @@
+<?php
+
+
+            $sayi = 4;
+            switch ($sayi):
+                case 4:
+                    echo "sayi değeri 4 dür.";
+                    break;
+                case 5:
+                    echo "sayi değeri 5 dir.";
+                    break;
+                default:
+                    echo "sayi değeri bilinmiyor.";
+                    break;
+            endswitch;
+
+            echo "<br>";
+
+            $a = 5;
+            if ($a == 5) :
+                echo "a değeri 5 tir.";
+            elseif ($a == 6) :
+                echo "a değeri 6 dır. ";
+            else :
+                echo "a değeri bilinmiyor";
+            endif;
+            echo "<br><br><br>"
+?>
+
+
+<?php
+    if ($a == 5) {
+?>
+        <b>a değeri 5 değerine eşittir.</b>
+<?php
+    }
+?>
