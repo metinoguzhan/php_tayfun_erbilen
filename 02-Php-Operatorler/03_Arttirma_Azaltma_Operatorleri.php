274d91ff41bd1340f045a6de1e0616a15f3d3446
unknown <metinoguzhann@gmail.com>
2020-09-26 23:51:30 +0300

    first commit

---
+++ 02-Php-Operatorler/03_Arttirma_Azaltma_Operatorleri.php
@@ -0,0 +1,26 @@
+<?php
+
+    /**
+     *          ARTTIRMA VE AZALTMA OPERATORLERI
+     * 
+     *  $a++
+     *  ++$a
+     *  $a--
+     *  --a$
+     * 
+     */
+
+
+    $a = 5;
+    echo $a;
+    echo "<br>";
+    echo $a++;
+    echo "<br>";
+    echo ++$a;
+    echo "<br>";
+    echo $a--;
+    echo "<br>";
+    echo --$a;
+    echo "<br>";
+
+?>
